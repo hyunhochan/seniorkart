e246b1f7c2fba9b71692b7702c4b96badf2c8779
Melonzui <robindalnim@naver.com>
2024-03-23 23:18:32 +0900

    Test

--- seniorkartProject/Assets/testScript.cs
+++ seniorkartProject/Assets/testScript.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//test.
 public class testScript : MonoBehaviour
 {
     // Start is called before the first frame update
