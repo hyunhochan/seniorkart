85590461bb0a78dec156b44fd93e1e7b4ce46c72
Melonzui <robindalnim@naver.com>
2024-03-25 12:31:30 +0900

    Firebase Create Account Test

--- seniorkartProject/Assets/firebaseTest.cs
+++ seniorkartProject/Assets/firebaseTest.cs
@@ -9,16 +9,16 @@ public class FirebaseAuthExample : MonoBehaviour
 
     void Start()
     {
-        // Firebase 인증 객체의 인스턴스를 초기화합니다.
+        
         auth = FirebaseAuth.DefaultInstance;
 
-        // 사용자를 등록하는 함수를 호출합니다.
+      
         //RegisterUser("user@example.com", "password123");
     }
 
     public void RegisterUser(string email, string password)
     {
-        // 이메일과 비밀번호를 사용하여 새 사용자를 생성합니다.
+        
         auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task => {
             if (task.IsCanceled)
             {
@@ -31,10 +31,10 @@ public class FirebaseAuthExample : MonoBehaviour
                 return;
             }
 
-            // 비동기 작업이 성공적으로 완료된 경우에만 FirebaseUser에 접근합니다.
+            
             if (task.IsCompleted)
             {
-                FirebaseUser newUser = task.Result.User; // AuthResult에서 User 속성을 통해 FirebaseUser에 접근합니다.
+                FirebaseUser newUser = task.Result.User;
                 Debug.LogFormat("Firebase user created successfully: {0} ({1})", newUser.DisplayName, newUser.UserId);
             }
         });
