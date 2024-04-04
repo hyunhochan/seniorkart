27fb12979393b0c41b20cb7c50bd4d8ab4ec4adc
hyunhochan <39771617+hyunhochan@users.noreply.github.com>
2024-04-04 13:33:42 +0900

    font added
    font added

--- seniorkartProject/Assets/Scripts/MainMove.cs
+++ seniorkartProject/Assets/Scripts/MainMove.cs
@@ -7,13 +7,20 @@ using DG.Tweening;
 public class MainMove : MonoBehaviour
 {
     public Button btn1;
-    public GameObject myEmptyGameObject;
+    public GameObject mainsequence;
+    public RectTransform mainRectTransform;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Button의 RectTransform을 가져옴
+        RectTransform rectTransform = btn1.GetComponent<RectTransform>();
+
+        // 높이와 너비를 출력
+        float btnwidth = rectTransform.rect.width;
+        float btnheight = rectTransform.rect.height;
+        mainRectTransform.DOAnchorPosX(btnwidth*2/3, 1f);
     }
 
     // Update is called once per frame
