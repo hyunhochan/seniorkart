6fbe2ba2e8dbb68d4d41ff227dff88c70ea41598
hyunhochan <39771617+hyunhochan@users.noreply.github.com>
2024-04-04 13:03:56 +0900

    install plugins
    installed plugins

---
+++ seniorkartProject/Assets/Scripts/MainMove.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class MainMove : MonoBehaviour
+{
+    public Button btn1;
+    public GameObject myEmptyGameObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
