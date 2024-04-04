88136fcebe61867a7e52b470b883ccebef15ee64
hyunhochan <39771617+hyunhochan@users.noreply.github.com>
2024-04-04 14:17:17 +0900

    dsaf
    asdf

--- seniorkartProject/Assets/Scripts/MainMove.cs
+++ seniorkartProject/Assets/Scripts/MainMove.cs
@@ -8,7 +8,9 @@ public class MainMove : MonoBehaviour
 {
     public Button btn1;
     public GameObject mainsequence;
+    public GameObject playsequence;
     public RectTransform mainRectTransform;
+    public RectTransform playRectTransform;
 
 
     // Start is called before the first frame update
@@ -20,12 +22,46 @@ public class MainMove : MonoBehaviour
         // 높이와 너비를 출력
         float btnwidth = rectTransform.rect.width;
         float btnheight = rectTransform.rect.height;
-        mainRectTransform.DOAnchorPosX(btnwidth*2/3, 1f);
+
+        mainRectTransform.DOAnchorPosX(btnwidth*2/3, 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void clickPlay()
+    {
+        // Button의 RectTransform을 가져옴
+        RectTransform rectTransform = btn1.GetComponent<RectTransform>();
+
+        // 높이와 너비를 출력
+        float btnwidth = rectTransform.rect.width;
+        float btnheight = rectTransform.rect.height;
+
+        mainRectTransform.DOAnchorPosX(0, 0.5f);
+        playRectTransform.DOAnchorPosX(btnwidth * 2 / 3, 0.5f);
+
+    }
+
+    public void clickPlayBack()
+    {
+        // Button의 RectTransform을 가져옴
+        RectTransform rectTransform = btn1.GetComponent<RectTransform>();
+
+        // 높이와 너비를 출력
+        float btnwidth = rectTransform.rect.width;
+        float btnheight = rectTransform.rect.height;
+
+        playRectTransform.DOAnchorPosX(0, 0.5f);
+        mainRectTransform.DOAnchorPosX(btnwidth * 2 / 3, 0.5f);
+
+    }
+
+    public void clickExit()
+    {
+        Application.Quit();
     }
 }
