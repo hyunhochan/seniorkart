391714f3f1f4618a93460ab1ce09472a2a282c81
hyunhochan <39771617+hyunhochan@users.noreply.github.com>
2024-04-26 13:50:05 +0900

    scripts linked
    scripts linked

---
+++ seniorkartProject/Assets/Scenes/scenetransition/toSinglePlay.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class toSinglePlay : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("room");
+
+    }
+}
