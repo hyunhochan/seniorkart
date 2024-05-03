c94843da45e87b3efdfaeb8916398d0d10c549c5
hyunhochan <39771617+hyunhochan@users.noreply.github.com>
2024-05-03 10:31:27 +0900

    added playsound
    added playsound

---
+++ seniorkartProject/Assets/UI/Racing/audioPitch.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class audioPitch : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    private AudioSource audioSource;
+    public AudioClip motorSound;
+
+    void Start()
+    {
+        
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = motorSound;
+
+        audioSource.loop = true;
+
+        
+        audioSource.Play();
+    }
+
+    void Update()
+    {
+        
+        Debug.Log(rigidbody.velocity.magnitude);
+
+        float pitch = Mathf.Lerp(0.5f, 2.0f, rigidbody.velocity.magnitude / 10.0f);
+        audioSource.pitch = pitch;
+    }
+}
