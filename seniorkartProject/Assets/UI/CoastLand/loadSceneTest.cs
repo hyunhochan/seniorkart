a5ffbc32d93585db9b02ade31700e6c4b8f01dd4
hyunhochan <39771617+hyunhochan@users.noreply.github.com>
2024-05-30 15:40:25 +0900

    push added btn
    push added btn

---
+++ seniorkartProject/Assets/UI/CoastLand/loadSceneTest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadSceneTest : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        LoadingSceneManager.LoadScene("Coastland");
+    }
+}
